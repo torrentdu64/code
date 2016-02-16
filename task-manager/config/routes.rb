a011f2e1977d6538a231c835e4c3d6bf34c538d7
torrent benoit <torrentdu64@gmail.com>
2016-02-16 13:48:19 +0100

    creation de controller avec erreur de syntax avec controller

--- task-manager/config/routes.rb
+++ task-manager/config/routes.rb
@@ -1,4 +1,18 @@
 Rails.application.routes.draw do
+  get 'tasks_controller/index'
+
+  get 'tasks_controller/show'
+
+  get 'tasks_controller/new'
+
+  get 'tasks_controller/create'
+
+  get 'tasks_controller/edit'
+
+  get 'tasks_controller/update'
+
+  get 'tasks_controller/destroy'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
